8baf0f756252b793a9f054068a78c90c61eb7c57
L. D. Nicolas May <ldnicolasmay@gmail.com>
2020-09-09 13:35:26 -0400

    Request fulfilled

--- 2020-09-08_Potempa_Election-Data.R
+++ 2020-09-08_Potempa_Election-Data.R
@@ -1,12 +1,19 @@
 # MICHIGAN BOARD OF ELECTIONS DATA 2020 ----
 
 # USEFUL LIBRARIES ----
-library(data.table)
-setDTthreads(3)
-library(magrittr)
+library(data.table); setDTthreads(3)
+library(pipeR)
+library(purrr)
+library(stringr)
+library(lubridate)
+library(readr)
 
 
-brd_elex_data_path <- 
+# EXTRACT ----
+
+# _ Define source data path ----
+
+data_dir <- 
   c(
     "",
     "Users",
@@ -18,9 +25,9 @@ brd_elex_data_path <-
     "Board of Elections data - Michigan"
   ) %>>% paste0(collapse = "/")
 
-# _ LOAD DATA ----
+# _ Define data columns ----
 
-cols_cnty_20 <-
+cols_county <-
   c(
     "LASTNAME",
     "FIRSTNAME",
@@ -61,10 +68,12 @@ cols_cnty_20 <-
     "VOTER_STATUS_TYPE_CODE",
     "UOVACAVA_STATUS_CODE"
   )
-cols_cnty_20_ <- cols_cnty_20
-cols_cnty_20_[cols_cnty_20_ == "VOTER_ID"] <- "VOTER_ID\""
+cols_county_ <- cols_county
+cols_county_[cols_county_ == "VOTER_ID"] <- "VOTER_ID\""
 
-counties <- c(
+# _ Define county names to iterate over ----
+
+counties_a <- c(
   "Livingston",
   "Macomb",
   "Monroe",
@@ -74,15 +83,7 @@ counties <- c(
   "Wayne"
 )
 
-fxn_fread <- function(path, select_cols, name_cols) {
-  fread(path, select = select_cols, col.names = name_cols, colClasses = "character")
-}
-
-df_county_2020_data <- counties %>>% 
-  (paste0(brd_elex_data_path, "/raw_data_2020/", ., " County.csv")) %>>% 
-  purrr::map_dfr(fxn_fread, select_cols = cols_cnty_20, name_cols = cols_cnty_20)
-
-counties_ <- c(
+counties_b <- c(
   "Alpena",
   "Bay",
   "Berrien",
@@ -97,11 +98,90 @@ counties_ <- c(
   "Saginaw"
 )
 
-df_county_2020_data_ <- counties_ %>>%
-  (paste0(brd_elex_data_path, "/raw_data_2020/", ., " QVF 8.2020.csv")) %>>%
-  purrr::map_dfr(fxn_fread, select_cols = cols_cnty_20_, name_cols = cols_cnty_20)
+# _ Read data ----
 
-df_county_2020 <- rbindlist(list(df_county_2020_data, df_county_2020_data_))
-rm(df_county_2020_data); rm(df_county_2020_data_)
+df_county_2020_a <- counties_a %>>% 
+  (paste0(data_dir, "/raw_data_2020/", ., " County.csv")) %>>% 
+  map_dfr(fread, select = cols_county, col.names = cols_county, colClasses = "character")
+
+df_county_2020_b <- counties_b %>>%
+  (paste0(data_dir, "/raw_data_2020/", ., " QVF 8.2020.csv")) %>>%
+  map_dfr(fread, select = cols_county_, col.names = cols_county, colClasses = "character")
+
+df_county_2020 <- rbindlist(list(df_county_2020_a, df_county_2020_b))
+
+# _ Clean up ----
+
+rm(df_county_2020_a); rm(df_county_2020_b)
+
+# _ Verify basic data facts ----
+# print(object.size(df_county_2020), units = "auto")
+# assertthat::are_equal(indices(df_county_2020), NULL)
 
 
+# TRANSFORM ----
+
+# _ Mutate ----
+
+# _ _ YOB as integer ----
+df_county_2020[, YOB := as.integer(YOB)]
+
+# _ _ REGISTRATION_DATE as date
+regex_date <- "\\d{1,2}/\\d{1,2}/\\d{2,4}"
+df_county_2020[, REGISTRATION_DATE := str_extract(REGISTRATION_DATE, regex_date)]
+df_county_2020[, REGISTRATION_DATE := mdy(regis_date_str)]
+
+# _ _ COUNTY_CODE as integer ----
+df_county_2020[, COUNTY_CODE := as.integer(COUNTY_CODE)]
+
+# _ _ ZIP_CODE as integer ----
+# (OK because no Michigan ZIPs start with 0s)
+df_county_2020[, ZIP_CODE := as.integer(ZIP_CODE)]
+
+# _ Filter ----
+
+# _ _ Filter for >= 50 years old and <= 100 years old ----
+this_year <- 2020L
+df_county_2020_flt <- df_county_2020[YOB <= this_year - 50L & YOB >= this_year - 100L, ]
+
+# _ Select fields ----
+
+df_county_2020_slc <- 
+  df_county_2020_flt[, .(VOTER_ID,
+                         FIRSTNAME, MIDDLENAME, LASTNAME, NAME_SUFFIX,
+                         STREET_NUMBER_PREFIX, STREET_NUMBER, STREET_NUMBER_SUFFIX,
+                         DIRECTION_PREFIX, STREET_NAME, STREET_TYPE, DIRECTION_SUFFIX,
+                         EXTENSION, CITY, STATE, ZIP_CODE)]
+# print(object.size(df_county_2020_slc), units = "auto")
+
+
+# LOAD ----
+
+# _ Get non-repeated random samples ----
+
+set.seed(1)
+df_county_2020_slc_resample <- df_county_2020_slc %>>% 
+  dplyr::sample_n(size = nrow(.), replace = FALSE)
+
+# _ Group resampled data into n=3000 and write to CSVs ----
+
+n <- 3000L
+target_dir <- paste0("./samples_2020_n", as.character(n))
+
+csv_nums <- 0:(floor(nrow(df_county_2020_slc_resample)/n))
+nchar_max <- nchar(max(csv_nums))
+
+# _ Get 
+walk(csv_nums,
+     function(num) {
+       csv_path <- paste0(target_dir, "/recruits_", str_pad(num, nchar_max, pad = "0"), ".csv")
+       first_row <- num * n + 1
+       last_row <- if (num * n <= nrow(df_county_2020_slc_resample)) { 
+         num * n + n 
+       } else {
+         nrow(df_county_2020_slc_resample)
+       }
+       
+       write_csv(df_county_2020_slc_resample[first_row:last_row, ], path = csv_path,
+                 na = "", append = FALSE, col_names = TRUE)
+     })
